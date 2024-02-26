bceb7cab5e5a323f1318306d6ef192f55090ff65
Dipalisabde <91582256+Dipalisabde@users.noreply.github.com>
2024-02-26 13:34:47 +0530

    update

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "detsdb");
+$con=mysqli_connect("money.mysql.database.azure.com", "money", "@Dipali123", "detsdb");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
