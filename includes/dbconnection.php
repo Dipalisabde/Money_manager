8f99c12caa4e9afd933e7619cfcdd0cdda328c4a
Dipalisabde <91582256+Dipalisabde@users.noreply.github.com>
2024-02-26 15:00:49 +0530

    update

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=new mysqli("money.mysql.database.azure.com ", "money", "@Dipali123", "money");
+$con=new mysqli("money.mysql.database.azure.com", "money", "@Dipali123", "money");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
