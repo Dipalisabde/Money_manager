9fcd55484b85629b664886f12a3244cc6e82999f
Dipalisabde <91582256+Dipalisabde@users.noreply.github.com>
2024-02-27 12:11:13 +0530

    update

--- dashboard.php
+++ dashboard.php
@@ -2,31 +2,33 @@
 session_start();
 error_reporting(0);
 include('includes/dbconnection.php');
-if (strlen($_SESSION['detsuid'] == 0)) {
-    header('location:logout.php');
-} else {
-?>
+if (strlen($_SESSION['detsuid']==0)) {
+  header('location:logout.php');
+  } else{
+
+  
+
+  ?>
 <!DOCTYPE html>
 <html>
-
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Money Manager</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/datepicker3.css" rel="stylesheet">
-    <link href="css/styles.css" rel="stylesheet">
-
-    <!--Custom Font-->
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-    <!--[if lt IE 9]>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Money Manager</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/font-awesome.min.css" rel="stylesheet">
+	<link href="css/datepicker3.css" rel="stylesheet">
+	<link href="css/styles.css" rel="stylesheet">
+	
+	<!--Custom Font-->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+	<!--[if lt IE 9]>
 	<script src="js/html5shiv.js"></script>
 	<script src="js/respond.min.js"></script>
 	<![endif]-->
     <style>
-        /* Style for chatbot button */
-        #chatbot-button {
+        /* Chatbot button style */
+        .chatbot-button {
             position: fixed;
             bottom: 20px;
             right: 20px;
@@ -39,14 +41,15 @@ if (strlen($_SESSION['detsuid'] == 0)) {
             font-size: 18px;
             cursor: pointer;
             box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+            z-index: 9999; /* Ensure button is above other elements */
         }
 
-        #chatbot-button:focus {
+        .chatbot-button:focus {
             outline: none;
         }
 
-        /* Style for chatbot container */
-        #chatbot-container {
+        /* Chatbot frame style */
+        .chatbot-frame {
             position: fixed;
             bottom: 80px;
             right: 20px;
@@ -58,9 +61,10 @@ if (strlen($_SESSION['detsuid'] == 0)) {
             border-radius: 5px;
             box-shadow: 0px 8px 10px rgba(0, 0, 0, 0.1);
             display: none;
+            z-index: 9999; /* Ensure frame is above other elements */
         }
 
-        #chatbot-header {
+        .chatbot-header {
             background-color: #007bff;
             color: #fff;
             padding: 10px;
@@ -68,18 +72,18 @@ if (strlen($_SESSION['detsuid'] == 0)) {
             border-top-right-radius: 5px;
         }
 
-        #chatbot-body {
+        .chatbot-body {
             padding: 15px;
         }
 
-        #chatbot-input {
+        .chatbot-input {
             width: calc(100% - 40px);
             padding: 10px;
             border: 1px solid #ccc;
             border-radius: 5px;
         }
 
-        #chatbot-send {
+        .chatbot-send {
             background-color: #007bff;
             color: #fff;
             border: none;
@@ -89,115 +93,221 @@ if (strlen($_SESSION['detsuid'] == 0)) {
             margin-left: 10px;
         }
 
-        #chatbot-send:hover {
+        .chatbot-send:hover {
             background-color: #0056b3;
         }
     </style>
 </head>
-
 <body>
+	
+	<?php include_once('includes/header.php');?>
+	<?php include_once('includes/sidebar.php');?>
+		
+	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+		<div class="row">
+			<ol class="breadcrumb">
+				<li><a href="#">
+					<em class="fa fa-home"></em>
+				</a></li>
+				<li class="active">Dashboard</li>
+			</ol>
+		</div><!--/.row-->
+		
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header">Dashboard</h1>
+			</div>
+		</div><!--/.row-->
+		
+		
+		
+		
+		<div class="row">
+			<div class="col-xs-6 col-md-3">
+				
+				<div class="panel panel-default">
+					<div class="panel-body easypiechart-panel">
+<?php
+//Today Expense
+$userid=$_SESSION['detsuid'];
+$tdate=date('Y-m-d');
+$query=mysqli_query($con,"select sum(ExpenseCost)  as todaysexpense from tblexpense where (ExpenseDate)='$tdate' && (UserId='$userid');");
+$result=mysqli_fetch_array($query);
+$sum_today_expense=$result['todaysexpense'];
+ ?> 
 
-    <?php include_once('includes/header.php'); ?>
-    <?php include_once('includes/sidebar.php'); ?>
+						<h4>Today's Expense</h4>
+						<div class="easypiechart" id="easypiechart-blue" data-percent="<?php echo $sum_today_expense;?>" ><span class="percent"><?php if($sum_today_expense==""){
+echo "0";
+} else {
+echo $sum_today_expense;
+}
 
-    <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
-        <div class="row">
-            <ol class="breadcrumb">
-                <li><a href="#">
-                        <em class="fa fa-home"></em>
-                    </a></li>
-                <li class="active">Dashboard</li>
-            </ol>
-        </div><!--/.row-->
+	?></span></div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-6 col-md-3">
+				<div class="panel panel-default">
+					<?php
+//Yestreday Expense
+$userid=$_SESSION['detsuid'];
+$ydate=date('Y-m-d',strtotime("-1 days"));
+$query1=mysqli_query($con,"select sum(ExpenseCost)  as yesterdayexpense from tblexpense where (ExpenseDate)='$ydate' && (UserId='$userid');");
+$result1=mysqli_fetch_array($query1);
+$sum_yesterday_expense=$result1['yesterdayexpense'];
+ ?> 
+					<div class="panel-body easypiechart-panel">
+						<h4>Yesterday's Expense</h4>
+						<div class="easypiechart" id="easypiechart-orange" data-percent="<?php echo $sum_yesterday_expense;?>" ><span class="percent"><?php if($sum_yesterday_expense==""){
+echo "0";
+} else {
+echo $sum_yesterday_expense;
+}
 
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">Dashboard</h1>
-            </div>
-        </div><!--/.row-->
+	?></span></div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-6 col-md-3">
+				<div class="panel panel-default">
+					<?php
+//Weekly Expense
+$userid=$_SESSION['detsuid'];
+ $pastdate=  date("Y-m-d", strtotime("-1 week")); 
+$crrntdte=date("Y-m-d");
+$query2=mysqli_query($con,"select sum(ExpenseCost)  as weeklyexpense from tblexpense where ((ExpenseDate) between '$pastdate' and '$crrntdte') && (UserId='$userid');");
+$result2=mysqli_fetch_array($query2);
+$sum_weekly_expense=$result2['weeklyexpense'];
+ ?>
+					<div class="panel-body easypiechart-panel">
+						<h4>Last 7day's Expense</h4>
+						<div class="easypiechart" id="easypiechart-teal" data-percent="<?php echo $sum_weekly_expense;?>"><span class="percent"><?php if($sum_weekly_expense==""){
+echo "0";
+} else {
+echo $sum_weekly_expense;
+}
 
-        <div class="row">
-            <div class="col-xs-6 col-md-3">
-                <div class="panel panel-default">
-                    <!-- Panel content for Today's Expense -->
-                </div>
-            </div>
-            <div class="col-xs-6 col-md-3">
-                <div class="panel panel-default">
-                    <!-- Panel content for Yesterday's Expense -->
-                </div>
-            </div>
-            <div class="col-xs-6 col-md-3">
-                <div class="panel panel-default">
-                    <!-- Panel content for Weekly Expense -->
-                </div>
-            </div>
-            <div class="col-xs-6 col-md-3">
-                <div class="panel panel-default">
-                    <!-- Panel content for Monthly Expense -->
-                </div>
-            </div>
-        </div><!--/.row-->
+	?></span></div>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-6 col-md-3">
+				<div class="panel panel-default">
+					<?php
+//Monthly Expense
+$userid=$_SESSION['detsuid'];
+ $monthdate=  date("Y-m-d", strtotime("-1 month")); 
+$crrntdte=date("Y-m-d");
+$query3=mysqli_query($con,"select sum(ExpenseCost)  as monthlyexpense from tblexpense where ((ExpenseDate) between '$monthdate' and '$crrntdte') && (UserId='$userid');");
+$result3=mysqli_fetch_array($query3);
+$sum_monthly_expense=$result3['monthlyexpense'];
+ ?>
+					<div class="panel-body easypiechart-panel">
+						<h4>Last 30day's Expenses</h4>
+						<div class="easypiechart" id="easypiechart-red" data-percent="<?php echo $sum_monthly_expense;?>" ><span class="percent"><?php if($sum_monthly_expense==""){
+echo "0";
+} else {
+echo $sum_monthly_expense;
+}
 
-        <div class="row">
-            <div class="col-xs-6 col-md-3">
-                <div class="panel panel-default">
-                    <!-- Panel content for Current Year Expenses -->
-                </div>
-            </div>
-            <div class="col-xs-6 col-md-3">
-                <div class="panel panel-default">
-                    <!-- Panel content for Total Expenses -->
-                </div>
-            </div>
-        </div>
+	?></span></div>
+					</div>
+				</div>
+			</div>
+		
+		</div><!--/.row-->
+			<div class="row">
+			<div class="col-xs-6 col-md-3">
+				<div class="panel panel-default">
+					<?php
+//Yearly Expense
+$userid=$_SESSION['detsuid'];
+ $cyear= date("Y");
+$query4=mysqli_query($con,"select sum(ExpenseCost)  as yearlyexpense from tblexpense where (year(ExpenseDate)='$cyear') && (UserId='$userid');");
+$result4=mysqli_fetch_array($query4);
+$sum_yearly_expense=$result4['yearlyexpense'];
+ ?>
+					<div class="panel-body easypiechart-panel">
+						<h4>Current Year Expenses</h4>
+						<div class="easypiechart" id="easypiechart-red" data-percent="<?php echo $sum_yearly_expense;?>" ><span class="percent"><?php if($sum_yearly_expense==""){
+echo "0";
+} else {
+echo $sum_yearly_expense;
+}
 
-        <!-- Chatbot button -->
-        <button id="chatbot-button" onclick="toggleChatbot()">Chat</button>
+	?></span></div>
 
-        <!-- Chatbot container -->
-        <div id="chatbot-container">
-            <div id="chatbot-header">
-                Chatbot
-            </div>
-            <div id="chatbot-body">
-                <!-- Chatbot messages will be displayed here -->
-            </div>
-            <div id="chatbot-input-container">
-                <input type="text" id="chatbot-input" placeholder="Type your message...">
-                <button id="chatbot-send" onclick="sendMessage()">Send</button>
-            </div>
-        </div>
 
-    </div>
-    <!--/.main-->
+					</div>
+				
+				</div>
 
-    <?php include_once('includes/footer.php'); ?>
-    <script src="js/jquery-1.11.1.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/chart.min.js"></script>
-    <script src="js/chart-data.js"></script>
-    <script src="js/easypiechart.js"></script>
-    <script src="js/easypiechart-data.js"></script>
-    <script src="js/bootstrap-datepicker.js"></script>
-    <script src="js/custom.js"></script>
+			</div>
 
+		<div class="col-xs-6 col-md-3">
+				<div class="panel panel-default">
+					<?php
+//Yearly Expense
+$userid=$_SESSION['detsuid'];
+$query5=mysqli_query($con,"select sum(ExpenseCost)  as totalexpense from tblexpense where UserId='$userid';");
+$result5=mysqli_fetch_array($query5);
+$sum_total_expense=$result5['totalexpense'];
+ ?>
+					<div class="panel-body easypiechart-panel">
+						<h4>Total Expenses</h4>
+						<div class="easypiechart" id="easypiechart-red" data-percent="<?php echo $sum_total_expense;?>" ><span class="percent"><?php if($sum_total_expense==""){
+echo "0";
+} else {
+echo $sum_total_expense;
+}
+
+	?></span></div>
+
+
+					</div>
+				
+				</div>
+
+			</div>
+
+
+		</div>
+		
+		<!--/.row-->
+	</div>	<!--/.main-->
+    <button class="chatbot-button" onclick="toggleChatbot()">Chat</button>
+    <iframe class="chatbot-frame" src="https://webchat.botframework.com/embed/moneybot-bot?s=ZFppcFlVTB0.gy0KU4hL0IBGPNMTiLpZk76jwUOYHIAJyBF12n8pv64" style="height: 502px; max-height: 502px;"></iframe>
+	
+	<?php include_once('includes/footer.php');?>
+	<script src="js/jquery-1.11.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/chart.min.js"></script>
+	<script src="js/chart-data.js"></script>
+	<script src="js/easypiechart.js"></script>
+	<script src="js/easypiechart-data.js"></script>
+	<script src="js/bootstrap-datepicker.js"></script>
+	<script src="js/custom.js"></script>
     <script>
         function toggleChatbot() {
-            var chatbotContainer = document.getElementById('chatbot-container');
-            if (chatbotContainer.style.display === 'none') {
-                chatbotContainer.style.display = 'block';
-            } else {
-                chatbotContainer.style.display = 'none';
-            }
+            var chatbotFrame = document.querySelector('.chatbot-frame');
+            chatbotFrame.style.display === 'none' ? chatbotFrame.style.display = 'block' : chatbotFrame.style.display = 'none';
         }
 
-        function sendMessage() {
-            // Code to send message to the chatbot
-            // This function will be called when the user clicks the send button
-        }
     </script>
-</body>
 
+	<script>
+		window.onload = function () {
+	var chart1 = document.getElementById("line-chart").getContext("2d");
+	window.myLine = new Chart(chart1).Line(lineChartData, {
+	responsive: true,
+	scaleLineColor: "rgba(0,0,0,.2)",
+	scaleGridLineColor: "rgba(0,0,0,.05)",
+	scaleFontColor: "#c5c7cc"
+	});
+};
+	</script>
+		
+</body>
 </html>
 <?php } ?>
